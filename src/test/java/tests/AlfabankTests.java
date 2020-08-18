c3bf6ac0e7d174a96871817258a760b04ab2a439
Svetlana <ck555555@yandex.ru>
2020-08-18 12:57:33 +0300

    Добавила Альфа Банк тест и билд Гредл

---
+++ src/test/java/tests/AlfabankTests.java
@@ -0,0 +1,19 @@
+package tests;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class AlfabankTests {
+        @Test
+        public void depozitTests() {
+                open("https:");
+
+        }
+
+}
+
+
+
