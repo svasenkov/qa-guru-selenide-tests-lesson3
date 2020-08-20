9e7573feb32dd4a55a43bf029908abe8e6b7dc80
Svetlana <ck555555@yandex.ru>
2020-08-20 12:29:12 +0300

    Добавила Альфа Банк тест и билд Гредл

--- src/test/java/tests/AlfabankTests.java
+++ src/test/java/tests/AlfabankTests.java
@@ -1,19 +1,29 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AlfabankTests {
         @Test
         public void depozitTests() {
-                open("https:");
+                open("https://alfabank.ru/");
+                $(byText("Вклады")).click();
+                $$(byText("Депозиты")).find(Condition.visible).click();
+                $$(byText("Архивные депозиты")).find(Condition.visible).click();
+                $$(".product-cell__row-wrapper row") .shouldHaveSize(3);
 
         }
 
-}
+
+        }
+
+
 
 
 
